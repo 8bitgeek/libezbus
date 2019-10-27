2b45f9f34de1fcf59317376f740dd86cf011daa4
Mike Sharkey <mike.sharkey@firmwaredone.com>
2019-10-27 00:12:23 -0400

    MIT License

--- ezapp/src/platform/stm32l431-pa2600/board.h
+++ ezapp/src/platform/stm32l431-pa2600/board.h
@@ -1,9 +1,26 @@
-/******************************************************************************
-* Copyright © 2016 by Pike Aerospace Research Corporation
-* All Rights Reserved
-******************************************************************************/
-#ifndef _PA2600_BOARD_H_
-#define _PA2600_BOARD_H_
+/*****************************************************************************
+* Copyright 2019 Mike Sharkey <mike.sharkey@mineairquality.com>              *
+*                                                                            *
+* Permission is hereby granted, free of charge, to any person obtaining a    *
+* copy of this software and associated documentation files (the "Software"), *
+* to deal in the Software without restriction, including without limitation  *
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,   *
+* and/or sell copies of the Software, and to permit persons to whom the      *
+* Software is furnished to do so, subject to the following conditions:       *
+*                                                                            *
+* The above copyright notice and this permission notice shall be included in *
+* all copies or substantial portions of the Software.                        *
+*                                                                            *
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR *
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   *
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL    *
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER *
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING    *
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER        *
+* DEALINGS IN THE SOFTWARE.                                                  *
+*****************************************************************************/
+#ifndef _STM32L431_BOARD_H_
+#define _STM32L431_BOARD_H_
 
 #include <caribou.h>
 #include <caribou/kernel/timer.h>
