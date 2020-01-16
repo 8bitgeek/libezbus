5000d98c525eee050648158e81c1b8dca3e8fe10
Mike Sharkey <mike@accelerated-systems.com>
2020-01-16 10:47:13 -0500

    copyright dates update

--- src/ezbus_port.h
+++ src/ezbus_port.h
@@ -1,5 +1,5 @@
 /*****************************************************************************
-* Copyright 2019 Mike Sharkey <mike.sharkey@mineairquality.com>              *
+* Copyright © 2019-2020 Mike Sharkey <mike.sharkey@mineairquality.com>       *
 *                                                                            *
 * Permission is hereby granted, free of charge, to any person obtaining a    *
 * copy of this software and associated documentation files (the "Software"), *
